6233d39b91b07d1063aaca90c0434e8305ac7936
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-10-29 16:49:46 +1100

    Add color in functionality upon mouseover in grid

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function generateGrid(gridSize) {
-
+function generateGrid(gridSize, itemSize) {
   const gridContainer = document.querySelector('.grid-container');
-
+  // gridContainer.setAttribute('display','flex');
+  // Generate square grid based on given grid size.
   for (let i = 0; i < gridSize; i++) {
     let gridRow = document.createElement('div');
-    gridRow.setAttribute('id','grid-row');
+    gridRow.setAttribute('class','grid-row');
+    gridRow.style.display = 'flex';
     for (let j = 0; j < gridSize; j++) {
       let gridItem = document.createElement('div');
-      gridItem.setAttribute('id','grid-item');
+      gridItem.classList.add('grid-item');
+      gridItem.style.height = gridItem.style.width = itemSize;
+      gridItem.addEventListener('mouseover', () => {
+        gridItem.classList.add('colored');
+      });
       gridRow.appendChild(gridItem);
     }
     gridContainer.appendChild(gridRow);
   }
+
 }
 
-generateGrid(16)
+generateGrid(64,'10px')
