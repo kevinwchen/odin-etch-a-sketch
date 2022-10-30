2973a1d92f9719be46e62b0c20c36f3334efa21d
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-10-30 17:46:01 +1100

    Fix border and grid positioning

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-function generateGrid(gridSize, itemSize) {
-  const gridContainer = document.querySelector('.grid-container');
-  // gridContainer.setAttribute('display','flex');
-  // Generate square grid based on given grid size.
-  for (let i = 0; i < gridSize; i++) {
+function getGridResolution() {
+  let gridRes = prompt("Enter a grid resolution (1 - 100):");
+  return gridRes;
+}
+
+function generateGrid(gridRes) {
+  const gridArea = document.querySelector('.grid-area');
+  let itemSize = gridSize / gridRes;
+
+  // Generate square grid based on given grid size.and resolution
+  for (let i = 0; i < gridRes; i++) {
     let gridRow = document.createElement('div');
     gridRow.setAttribute('class','grid-row');
     gridRow.style.display = 'flex';
-    for (let j = 0; j < gridSize; j++) {
+    for (let j = 0; j < gridRes; j++) {
       let gridItem = document.createElement('div');
       gridItem.classList.add('grid-item');
-      gridItem.style.height = gridItem.style.width = itemSize;
+      gridItem.style.height = gridItem.style.width = `${itemSize}px`;
       gridItem.addEventListener('mouseover', () => {
         gridItem.classList.add('colored');
       });
       gridRow.appendChild(gridItem);
     }
-    gridContainer.appendChild(gridRow);
+    gridArea.appendChild(gridRow);
   }
 
 }
 
-generateGrid(64,'10px')
+const sizeBtn = document.querySelector('#resBtn');
+const resBtn = document.querySelector('#resBtn');
+
+// Set default grid size in px
+let gridSize = 600;
+// Set default grid resolution
+let gridRes = 64;
+
+generateGrid(gridRes);
+
+
