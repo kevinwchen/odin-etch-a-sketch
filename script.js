c3a25a69825facf6bc403a5ff6b84f9135ffcf02
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-10-31 08:30:33 +1100

    Fix change size feature and formatting

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+function getGridSize() {
+  let gridSize = prompt("Enter a grid size (1 - 600):");
+  if (gridSize == null || gridSize < 1 || gridSize > 1000) {
+    getGridResolution();
+  }
+  return gridSize;
+}
+
 function getGridResolution() {
   let gridRes = prompt("Enter a grid resolution (1 - 100):");
+  if (gridRes == null || gridRes < 1 || gridRes > 100) {
+    getGridResolution();
+  }
   return gridRes;
 }
 
-function generateGrid(gridRes) {
-  const gridArea = document.querySelector('.grid-area');
+function clearGrid() {
+  gridArea.innerHTML = "";
+}
+
+function generateGrid() {
+  // Set grid item size based on total grid size and resolution
   let itemSize = gridSize / gridRes;
 
   // Generate square grid based on given grid size.and resolution
@@ -12,28 +27,45 @@ function generateGrid(gridRes) {
     let gridRow = document.createElement('div');
     gridRow.setAttribute('class','grid-row');
     gridRow.style.display = 'flex';
+
     for (let j = 0; j < gridRes; j++) {
       let gridItem = document.createElement('div');
       gridItem.classList.add('grid-item');
       gridItem.style.height = gridItem.style.width = `${itemSize}px`;
+
       gridItem.addEventListener('mouseover', () => {
         gridItem.classList.add('colored');
       });
+
       gridRow.appendChild(gridItem);
     }
     gridArea.appendChild(gridRow);
   }
-
 }
 
-const sizeBtn = document.querySelector('#resBtn');
+const sizeBtn = document.querySelector('#sizeBtn');
 const resBtn = document.querySelector('#resBtn');
+const gridArea = document.querySelector('.grid-area');
 
-// Set default grid size in px
-let gridSize = 600;
-// Set default grid resolution
-let gridRes = 64;
+let gridSize = 600; // Set default grid size in px
+let gridRes = 64; // Set default grid resolution
 
-generateGrid(gridRes);
+sizeBtn.addEventListener('click', () => {
+  gridSize = getGridSize();
+  clearGrid();
+  gridArea.style.width = `${gridSize}px`;
+  gridArea.style.maxWidth = `${gridSize}px`;
+  gridArea.style.height = `${gridSize}px`;
+  gridArea.style.maxHeight = `${gridSize}px`;
+  generateGrid();
+});
+
+resBtn.addEventListener('click', () => {
+  gridRes = getGridResolution();
+  clearGrid();
+  generateGrid();
+});
+
+generateGrid();
 
 
